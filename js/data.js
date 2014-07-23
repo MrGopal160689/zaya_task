35fd65bac9f72edb8b6fa7cce499fcbb6bb93910
MrGopal160689 <gopal.virtual@gmail.com>
2014-07-23 21:34:16 +0530

    style changed

--- js/data.js
+++ js/data.js
@@ -3,5 +3,9 @@ var lesson = [
 {id : 2, title : "Advanced Lorem Ipsum", subject : "Math", grade : 2, noOfVideos : 1, noOfDocuments : 0, noOfQuestions : 1},
 {id : 3, title : "Lorem Ipsum", subject : "English", grade : 1, noOfVideos : 0, noOfDocuments : 1, noOfQuestions : 1},
 {id : 4, title : "Lorem Ipsum", subject : "Science", grade : 1, noOfVideos : 1, noOfDocuments : 1, noOfQuestions : 2},
-{id : 5, title : "Lorem Ipsum", subject : "Computers", grade : 2, noOfVideos : 1, noOfDocuments : 1, noOfQuestions : 1}
+{id : 5, title : "Lorem Ipsum", subject : "Computers", grade : 2, noOfVideos : 1, noOfDocuments : 1, noOfQuestions : 1},
+{id : 6, title : "Lorem Ipsum", subject : "Biology", grade : 3, noOfVideos : 9, noOfDocuments : 22, noOfQuestions : 0},
+{id : 7, title : "Lorem Ipsum", subject : "Java", grade : 2, noOfVideos : 203, noOfDocuments : 0, noOfQuestions : 1},
+{id : 8, title : "Lorem Ipsum", subject : "Automata", grade : 2, noOfVideos : 1, noOfDocuments : 1, noOfQuestions : 1},
+{id : 9, title : "Lorem Ipsum", subject : "Geometry", grade : 2, noOfVideos : 999, noOfDocuments : 1000, noOfQuestions : 100000}
 ];
